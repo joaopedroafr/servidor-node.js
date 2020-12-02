467a5c30fd5c7d5f3f44922aea7f65fe184b36ab
João Pedro <joaopedroafresende@gmail.com>
2020-12-02 17:32:46 -0300

    Primeiro json

--- hello.js
+++ hello.js
@@ -4,8 +4,9 @@ const host = "localhost";
 const port = 8080;
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+	res.setHeader("Content-Type", "application/json");
+	res.writeHead(200);
+    res.end(`{"message": "This is a JSON response"}`);
 };
 
 
