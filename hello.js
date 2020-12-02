8ff5707909d89d6314c84dc7f256b80f339d7d2d
João Pedro <joaopedroafresende@gmail.com>
2020-12-02 15:43:42 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,16 +3,15 @@ const http =  require("http");
 const host = "localhost";
 const port = 8080;
 
-const requestListener = (req, res) => {
-	res = writeHed(200),
-		res = ("My Firs Server!");
-
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
 };
+
 
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log("Server is running on http://${host}:${port}");
+    console.log(`Server is running on http://${host}:${port}`);
 });
-
 
