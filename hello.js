19dd3574264146db203c9e7bf20f41da1b2f4314
João Pedro <joaopedroafresende@gmail.com>
2020-12-02 15:32:43 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,9 +3,9 @@ const http =  require("http");
 const host = "localhost";
 const port = 8080;
 
-const requestListener = Function (req, res) {
-	res.writeHed(200);
-	res.end("My Firs Server!");
+const requestListener = (req, res) => {
+	res = writeHed(200),
+		res = ("My Firs Server!");
 
 };
 
